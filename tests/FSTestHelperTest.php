4f362487e29a222994a08bb2bf6594a286c3cfdf
Simon Jodet <simon.jodet@f-secure.com>
2012-10-03 14:14:53 +0200

    Adding an initial TODO list

--- tests/FSTestHelperTest.php
+++ tests/FSTestHelperTest.php
@@ -2,6 +2,13 @@
 
 require_once __DIR__ . '/../FSTestHelper/FSTestHelper.php';
 
+
+/**
+ * TODO: Add a method that creates the temporary folder
+ * TODO: Add a method that recursively create a path
+ * TODO: Add a method that recursively lists folders and files
+ * TODO: Add a method that creates folders and files based on an array or JSON string to a specified location
+ */
 class FSTestHelperTest extends \PHPUnit_Framework_TestCase
 {
     public function test_nothing()
