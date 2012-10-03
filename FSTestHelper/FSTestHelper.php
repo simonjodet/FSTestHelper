5c418fcda27ad71f7790d1d1ed41a42d0ce9c784
Simon Jodet <simon@jodet.com>
2012-10-03 14:55:27 +0200

    Construtor creates unique test folders

--- FSTestHelper/FSTestHelper.php
+++ FSTestHelper/FSTestHelper.php
@@ -11,7 +11,24 @@ namespace FSTestHelper;
  */
 class FSTestHelper
 {
+    private $path;
 
+    public function __construct()
+    {
+        $i = 0;
+        $this->path = realpath(sys_get_temp_dir()) . '/test_' . $i;
+        while (file_exists($this->path))
+        {
+            $i++;
+            $this->path = realpath(sys_get_temp_dir()) . '/test_' . $i;
+        }
+        mkdir($this->path);
+    }
+
+    public function getPath()
+    {
+        return $this->path;
+    }
 }
 
 /**
--- tests/FSTestHelperTest.php
+++ tests/FSTestHelperTest.php
@@ -4,15 +4,28 @@ require_once __DIR__ . '/../FSTestHelper/FSTestHelper.php';
 
 
 /**
- * TODO: Add a method that creates the temporary folder
  * TODO: Add a method that recursively create a path
  * TODO: Add a method that recursively lists folders and files
  * TODO: Add a method that creates folders and files based on an array or JSON string to a specified location
  */
 class FSTestHelperTest extends \PHPUnit_Framework_TestCase
 {
-    public function test_nothing()
+    public function test_constructor_returns_a_temporary_folder_in_the_system_temporary_folder()
     {
+        $systemTempFolder = realpath(sys_get_temp_dir());
+        $FSTestHelper = new \FSTestHelper\FSTestHelper();
+        $this->assertStringStartsWith($systemTempFolder . '/', $FSTestHelper->getPath());
     }
+
+    public function test_constructor_returns_a_unique_temporary_folder()
+    {
+        $FSTestHelper = new \FSTestHelper\FSTestHelper();
+        $path1 = $FSTestHelper->getPath();
+        $FSTestHelper = new \FSTestHelper\FSTestHelper();
+        $path2 = $FSTestHelper->getPath();
+
+        $this->assertNotEquals($path1, $path2);
+    }
+
 }
 
